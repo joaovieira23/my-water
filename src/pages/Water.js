3accb59ccfbcea5cd7628cff1a08821f7bf78513
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-31 19:29:07 -0300

    feat: add text input

--- src/pages/Water.js
+++ src/pages/Water.js
@@ -3,9 +3,7 @@ import { Text ,StyleSheet, TouchableOpacity, Picker } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialIcons } from '@expo/vector-icons';
 // import { useNavigation } from '@react-navigation/native'
-
-
-import bannerImg from '../assets/banner.png';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 export default function Water(props) {
   // const navigation = useNavigation();
@@ -21,6 +19,14 @@ export default function Water(props) {
      >
         <Text style={styles.title}>Add the amount of drink</Text>
 
+        <AnimatedCircularProgress
+          size={300}
+          width={40}
+          fill={100}
+          tintColor="#75A1DE"
+          backgroundColor="#fff"
+        />
+
 
         <Picker
           style={styles.picker}
@@ -33,19 +39,19 @@ export default function Water(props) {
           }
           itemStyle={styles.itemPicker}
         >
-          <Picker.Item  label="30 ml" value="30 ml" />
-          <Picker.Item label="50 ml" value="50 ml" />
-          <Picker.Item label="100 ml" value="100 ml" />
-          <Picker.Item label="150 ml" value="150 ml" />
-          <Picker.Item label="200 ml" value="200 ml" />
-          <Picker.Item label="250 ml" value="250 ml" />
-          <Picker.Item label="300 ml" value="300 ml" />
-          <Picker.Item label="330 ml" value="330 ml" />
-          <Picker.Item label="400 ml" value="400 ml" />
-          <Picker.Item label="500 ml" value="500 ml" />
-          <Picker.Item label="600 ml" value="600 ml" />
-          <Picker.Item label="800 ml" value="800 ml" />
-          <Picker.Item label="1000 ml" value="1000 ml" />
+          <Picker.Item  label="30 ml" value="30" />
+          <Picker.Item label="50 ml" value="50" />
+          <Picker.Item label="100 ml" value="100" />
+          <Picker.Item label="150 ml" value="150" />
+          <Picker.Item label="200 ml" value="200" />
+          <Picker.Item label="250 ml" value="250" />
+          <Picker.Item label="300 ml" value="300" />
+          <Picker.Item label="330 ml" value="330" />
+          <Picker.Item label="400 ml" value="400" />
+          <Picker.Item label="500 ml" value="500" />
+          <Picker.Item label="600 ml" value="600" />
+          <Picker.Item label="800 ml" value="800" />
+          <Picker.Item label="1000 ml (1 Litro)" value="1000 ml" />
         </Picker>
 
         <TouchableOpacity style={styles.button} onPress={handleSubmit}>
@@ -69,7 +75,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     maxWidth: 300,
     marginVertical: 20,
-    paddingBottom: 350
+    paddingBottom: 50
   },
 
   button: {
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Text ,StyleSheet, Image, TouchableOpacity } from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { Text ,StyleSheet, Image, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 
 import bannerImg from '../assets/banner.png';
 
 export default function Welcome() {
+  const[weightQuantity, setweightQuantity] = useState('');
   const navigation = useNavigation();
 
   function navigateToWater() {
@@ -14,16 +14,20 @@ export default function Welcome() {
   }
 
   return  (
-     <LinearGradient
-        colors={['#282F68', '#2A3379']}
-        style={styles.container}
-     >
-        <Image style={styles.banner} source={bannerImg}/>
-        <Text style={styles.title}>Control your daily water intake</Text>
-        <TouchableOpacity style={styles.button} onPress={navigateToWater}>
-          <MaterialIcons name="opacity" size={32} color="#FFF"/>
-        </TouchableOpacity>
-    </LinearGradient>
+        <KeyboardAvoidingView style={styles.container} behavior='position'>
+          <Image style={styles.banner} source={bannerImg}/>
+          <Text style={styles.title}>Control your daily water intake</Text>
+          <Text style= {styles.enterInput}>Enter your weight or the amount of water you want to drink each day</Text>
+          <TextInput
+            keyboardType='numeric'
+            style={styles.input}
+            placeholder='weight or quantity'
+            onChangeText={(val) => setweightQuantity(val)}
+            />
+          <TouchableOpacity style={styles.button} onPress={navigateToWater}>
+            <MaterialIcons name="opacity" size={32} color="#FFF"/>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
   );
 }
 
@@ -31,12 +35,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#282F68',
   },
 
   banner: {
     alignSelf: 'center',
-    alignItems: 'center'
   },
 
   title: {
@@ -49,12 +53,31 @@ const styles = StyleSheet.create({
     paddingBottom: 20
   },
 
+  enterInput: {
+    color: '#FFDEAD',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    maxWidth: 300,
+    marginVertical: 20
+  },
+
   button: {
     width: 72,
     height: 72,
     backgroundColor: '#2e5b9a',
-    borderRadius: 36,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    alignSelf: 'center',
+    borderRadius: 36,
+    marginVertical: 30
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 8,
+    margin: 10,
+    width: 300
+  },
 })
